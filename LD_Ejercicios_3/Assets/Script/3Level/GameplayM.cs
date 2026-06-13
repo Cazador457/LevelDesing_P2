74244a278d75c17a1562448b2657fd27de328c1f
Angel Bernardino <chiv_157@hotmail.com>
2026-06-13 00:13:57 -0600

    last

---
+++ LD_Ejercicios_3/Assets/Script/3Level/GameplayM.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameplayM : MonoBehaviour
+{
+    public Vector2 ReturnPos;
+    public Vector2 Player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
